e402d6e33493805a9a29d17b92bca7b30ec078ad
aik <anthonykrueger0@gmail.com>
2021-05-07 13:04:57 -0500

    started work on local storage

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function makeScheduler () {
     for(i = 0; i < 9; i++){
         var row = $("<div></div>").addClass("row");
         var timeLabel = $("<span></span>").addClass("col-1 span6");
-        var inputField = $("<textarea class='span6 col-10' rows='2' required></textarea>").attr('index', i);
-        var saveButton = $("<button class ='btn btn-primary col-1'>Save</button>").attr('index', i);
+        var inputField = $("<textarea class='span6 col-10 todos' rows='2' required></textarea>").data('index', i);
+        var saveButton = $("<button class ='btn btn-primary col-1 savebtn'>Save</button>").data('index', i);
 
         if(i <= 2){
             timeLabel.text((i + 9) + "AM")
@@ -26,13 +26,13 @@ function makeScheduler () {
         }
         var currentHour = moment().toObject().hours;
         if(currentHour < i + 9) {
-            inputField.css('background-color', 'green')
+            inputField.css('background-color', 'lime')
         }
         else if(currentHour == i + 9) {
             inputField.css('background-color', 'red')
         }
         else {
-            inputField.css('background-color', 'gray')
+            inputField.css('background-color', 'lightgrey')
         }
         schedulerList.append(row);
         row.append(timeLabel);
@@ -41,5 +41,35 @@ function makeScheduler () {
 
     }
 }
+
+// Give all save-buttons click functions
+
+$(".savebtn").on("click", function() {
+    console.log($(this).data("index"));
+})
+
+// Create localstorage array if not already present
+
+function checkStorage() {
+    var unparsedstore = localStorage.getItem('todos')
+    if(unparsedstore == null) {
+        var newstore = ["", "", "", "", "", "", "", "", ""]
+        localStorage.setItem('todos', JSON.stringify(newstore))
+    }
+    else {
+        var inputs = $('.todos')
+        var store = JSON.parse(localStorage.getItem('todos'))
+        for(i = 0; i < inputs.length; i++){
+            var currentInput = inputs[i];
+            var currentData = store[i];
+            if(currentData != null) {
+                currentInput.textContent = currentData;
+            }
+        }
+    }
+}
+var teststore = ["thingtodo", "", "", "", "", "another", "", "", ""]
+localStorage.setItem('todos', JSON.stringify(teststore))
 setDate();
 makeScheduler();
+checkStorage();
